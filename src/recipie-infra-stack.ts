4b4832b896dd04e2707316c00017a08eba8cbc66
Oscar C <oacs1221@gmail.com>
2023-03-15 12:35:45 -0400

    chore: add eslint

--- src/recipie-infra-stack.ts
+++ src/recipie-infra-stack.ts
@@ -1,9 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class RecipieInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class RecipieInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
